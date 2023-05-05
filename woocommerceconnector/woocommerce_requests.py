20a46ddaa87502ed3cd2ba572789b6995d41a818
SafdarAliGit <safdar211@gmail.com>
2023-05-05 21:48:08 +0500

    wooo changed

--- woocommerceconnector/woocommerce_requests.py
+++ woocommerceconnector/woocommerce_requests.py
@@ -176,9 +176,9 @@ def get_woocommerce_items(ignore_filter_conditions=False):
     response = get_request_request('products?per_page={0}&{1}'.format(_per_page,filter_condition) )
     woocommerce_products.extend(response.json())
 
-    for page_idx in range(1, int( response.headers.get('X-WP-TotalPages')) or 1):
-        response = get_request_request('products?per_page={0}&page={1}&{2}'.format(_per_page,page_idx+1,filter_condition) )
-        woocommerce_products.extend(response.json())
+    # for page_idx in range(1, int( response.headers.get('X-WP-TotalPages')) or 1):
+    #     response = get_request_request('products?per_page={0}&page={1}&{2}'.format(_per_page,page_idx+1,filter_condition) )
+    #     woocommerce_products.extend(response.json())
 
     return woocommerce_products
 
